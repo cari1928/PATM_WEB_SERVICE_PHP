fd676b0ff42240024a22a25b9b45a0edae08c3d7
cari1928 <13030622@itcelaya.edu.mx>
2017-03-04 01:11:09 -0600

    Token Local

--- controller/WSLocal.php
+++ controller/WSLocal.php
@@ -6,10 +6,18 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 /**
  * GET ALL LOCALES
  */
-$app->get('/api/local/listado', function (Request $request, Response $response) {
+$app->get('/api/local/listado/{nomUsr}/{pass}/{token}', function (Request $request, Response $response) {
   try {
-    $web     = new Local;
-    $locales = $web->getListadoL();
+    $bitacora = new Bitacora;
+    $bitacora->setUsuario($request->getAttribute('nomUsr'));
+    $bitacora->setPass($request->getAttribute('pass'));
+    $bitacora->setToken($request->getAttribute('token'));
+
+    $locales = array('status'=>"No se pudo obtener el listado");
+    if($bitacora->validaToken()) {
+      $web     = new Local;
+      $locales = $web->getListadoL();
+    }
 
     return $response->withStatus(200)
       ->withHeader('Content-Type', 'application/json')
@@ -23,12 +31,20 @@ $app->get('/api/local/listado', function (Request $request, Response $response) 
 /**
  * GET SINGLE LOCAL
  */
-$app->get('/api/local/{id}', function (Request $request, Response $response) {
+$app->get('/api/local/{id}/{nomUsr}/{pass}/{token}', function (Request $request, Response $response) {
   try {
-    $id  = $request->getAttribute('id');
-    $web = new Local;
-    $web->setId($id);
-    $local = $web->getLocal();
+    $bitacora = new Bitacora;
+    $bitacora->setUsuario($request->getAttribute('nomUsr'));
+    $bitacora->setPass($request->getAttribute('pass'));
+    $bitacora->setToken($request->getAttribute('token'));
+
+    $local = array('status'=>"No se pudo obtener el local");
+    if($bitacora->validaToken()) {
+      $id  = $request->getAttribute('id');
+      $web = new Local;
+      $web->setId($id);
+      $local = $web->getLocal();
+    }
 
     return $response->withStatus(200)
       ->withHeader('Content-Type', 'application/json')
@@ -42,18 +58,25 @@ $app->get('/api/local/{id}', function (Request $request, Response $response) {
 /**
  * POST ADD LOCAL
  */
-$app->post('/api/local/add', function (Request $request, Response $response) {
+$app->post('/api/local/add/{nomUsr}/{pass}/{token}', function (Request $request, Response $response) {
   try {
-    $datos = array(
-      'area'       => $request->getParam('area'),
-      'almacen'    => $request->getParam('almacen'),
-      'renta_base' => $request->getParam('renta_base'),
-    );
-    //llave foranea pendiente!!!
+    $bitacora = new Bitacora;
+    $bitacora->setUsuario($request->getAttribute('nomUsr'));
+    $bitacora->setPass($request->getAttribute('pass'));
+    $bitacora->setToken($request->getAttribute('token'));
 
-    $web = new Local;
-    $web->setDatos($datos);
-    $local = $web->insLocal();
+    $local = array('status'=>"No se pudo añadir");
+    if($bitacora->validaToken()) {
+      $datos = array(
+        'area'       => $request->getParam('area'),
+        'almacen'    => $request->getParam('almacen'),
+        'renta_base' => $request->getParam('renta_base'),
+      );
+
+      $web = new Local;
+      $web->setDatos($datos);
+      $local = $web->insLocal();
+    }
 
     return $response->withStatus(200)
       ->withHeader('Content-Type', 'application/json')
@@ -67,20 +90,27 @@ $app->post('/api/local/add', function (Request $request, Response $response) {
 /**
  * PUT UPDATE LOCAL
  */
-$app->put('/api/local/update', function (Request $request, Response $response) {
+$app->put('/api/local/update/{nomUsr}/{pass}/{token}', function (Request $request, Response $response) {
   try {
-    $id    = $request->getParam('id');
-    $datos = array(
-      'id'         => $id,
-      'area'       => $request->getParam('area'),
-      'almacen'    => $request->getParam('almacen'),
-      'renta_base' => $request->getParam('renta_base'),
-    );
-    //llave foranea pendiente!!!
+    $bitacora = new Bitacora;
+    $bitacora->setUsuario($request->getAttribute('nomUsr'));
+    $bitacora->setPass($request->getAttribute('pass'));
+    $bitacora->setToken($request->getAttribute('token'));
 
-    $web = new Local;
-    $web->setDatos($datos);
-    $local = $web->updLocal();
+    $local = array('status'=>"No se pudo actualizar");
+    if($bitacora->validaToken()) {
+      $id    = $request->getParam('id');
+      $datos = array(
+        'id'         => $id,
+        'area'       => $request->getParam('area'),
+        'almacen'    => $request->getParam('almacen'),
+        'renta_base' => $request->getParam('renta_base'),
+      );
+
+      $web = new Local;
+      $web->setDatos($datos);
+      $local = $web->updLocal();
+    }
 
     return $response->withStatus(200)
       ->withHeader('Content-Type', 'application/json')
@@ -94,18 +124,26 @@ $app->put('/api/local/update', function (Request $request, Response $response) {
 /**
  * DELETE LOCAL
  */
-$app->delete('/api/local/delete/{id}', function (Request $request, Response $response) {
+$app->delete('/api/local/delete/{id}/{nomUsr}/{pass}/{token}', function (Request $request, Response $response) {
   try {
-    $id = $request->getAttribute('id');
+    $bitacora = new Bitacora;
+    $bitacora->setUsuario($request->getAttribute('nomUsr'));
+    $bitacora->setPass($request->getAttribute('pass'));
+    $bitacora->setToken($request->getAttribute('token'));
 
-    $web = new Local;
-    $web->conexion();
-    $web->setId($id);
-    $web->delLocal();
+    $local = array('status'=>"No se pudo eliminar");
+    if($bitacora->validaToken()) {
+      $id = $request->getAttribute('id');
+      $web = new Local;
+      $web->conexion();
+      $web->setId($id);
+      $web->delLocal();
+      $local = array('status:'=>"Eliminado");
+    }
 
     return $response->withStatus(200)
       ->withHeader('Content-Type', 'application/json')
-      ->write('{"notice" : {"text" : "Eliminado"}}');
+      ->write(json_encode($local));
 
   } catch (PDOException $e) {
     echo '{"error" : {"text" : ' . $e->getMessage() . '}}';
